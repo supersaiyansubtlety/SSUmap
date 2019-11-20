c160d7771f364ad562e45cb97d2532aca9de4e06
Will Lucic <wlucic@blue.cs.sonoma.edu>
2019-11-20 11:55:51 -0800

    cleaned things up

--- index.js
+++ index.js
@@ -56,26 +56,8 @@ function BackButtonLMB(d)
 
 function BuildingHandlerLMB(bound)
 {
-    // Only scale if current scale is maxed out.
-    //var newX = -x * c.maxZoomScale;
-    //var newY = -y * c.maxZoomScale;
-    //console.log("Old X = ", x, " and old Y = ", y);
+    console.log('id: ', bound.attr('id'))
 
-    //console.log("BuildingHandler entered: ", building, newX, newY);
-    // Zoom into the building that was clicked, either by manually setting the translate or
-    // making a call using D3's zoom function.
-    console.log('id: ', d3.select(bound).attr('id'))
-    // var id = bound.attr('id')
-    // console.log('building id: ', id)
-
-    // var bounds = d3.select(building).select('rect');
-    // console.log("Bounds: ", bounds.node());
-    // newX = bounds.node().x.animVal.value * -1;
-    // newY = bounds.node().y.animVal.value * -1;
-    // console.log("New X = ", newX, " and new Y = ", newY);
-    // console.log("building:", building);
-    // console.log("boundParent: ", d3.select(bounds.node().parentNode));
-    // d3.select(building).call(zoom.transform, `translate(${newX}, ${newY}) scale(${c.maxZoomScale})`);
 }
 
 function makeClickables()
@@ -83,63 +65,16 @@ function makeClickables()
     // var sel = d3.selectAll('#Buidlings g')
     var sel = d3.select('svg').selectAll('*').filter(function(d, dd, ddd)
     {
-      // console.log('d: ', d)
-      // console.log('dd: ', dd)
-      // console.log('ddd: ', ddd)
-      // console.log('ddd[dd]: ', ddd[dd])
       id = d3.select(this).attr('id')
       if((id) && (id.indexOf('-Bounds') !== -1) && (id.indexOf('-Bounds') === (id.length - 7)))
-      {
-        // console.log('bound: ', id)
-        // d3.select(this).on('click', function(dd)
-        // {
-        //   console.log('clicking');
-        //   BuildingHandlerLMB(this);
-        // });
         return true
-      }
       return false
     })
     console.log('sel: ', sel)
 
-    sel._groups[0].forEach(function (d)
+    sel.on('click', function (e, i, p)
     {
-      // console.log('d: ', d3.select(d))
-      d3.select(d).on('click', function (e, i, p)
-      {
-        console.log('i: ', i)
-        console.log('p: ', p)
-        console.log('p[i]: ', p[i])
-        console.log('clicked this: ', d3.select(this))
-        // BuildingHandlerLMB(this)
-      })
-      // .attr('opacity', 0)
+      // console.log('clicked this: ', d3.select(this))
+      BuildingHandlerLMB(d3.select(this))
     })
-
-//     sel.on('click', function(d) {
-// 	// var x = d3.mouse(this)[0];
-// 	// var y = d3.mouse(this)[1];
-//
-// // console.log(d3.select(this))
-//
-//   // if (id.indexOf('-Bounds') == -1)
-//   // {
-//   //   console.log('not a bound')
-//   //   return
-//   // }
-//   // console.log('this is a bound')
-//
-// 	BuildingHandlerLMB(this);
-//     });
-
-    // DEBUG Clickable
-    // Only used to manually determine coordinates
-    /*
-    var coordSel = d3.select('body svg');
-    coordSel.on('click', function(d) {
-	var a = d3.mouse(this)[0];
-	var b = d3.mouse(this)[1];
-	console.log("Body-SVG Coords: (", a, ",", b, ")");
-    });
-    */
 }
