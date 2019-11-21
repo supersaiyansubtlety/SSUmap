b41946ce96be58edb96cfb4bdb11492f99d01b17
mbaello <33732855+mbaello@users.noreply.github.com>
2019-11-20 23:17:23 -0800

    (Mostly) Correct Mouse Coordinates
    -The mouse coordinates now closely match the intended rect coordinates of the SVG.
    -Modified the SVG a little and added more rects (I figure I can just work on this as an aside to "standardize" the process. i.e. only rects).
    -1.5 seems like a nice factor for zooming. Not sure why.

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ const c = {
     mapHeight: 800,
     minZoomScale: 1,
     maxZoomScale: 6,
+    svgVBWidth: 3300,
+    svgVBHeight: 2550,
+    zDuration: 1000,
+    zEase: d3.easeCubicOut
 };
 
 var zoom;
+var mapSVG;
+var mouseX;
+var mouseY;
 
 function loadSVG()
 {
     // Resize body.
     // Call D3's zoom function which also handles translating.
-    // Not sure if this will conflict with clicks.
+    // Not sure if this will conflict with clicks.   	
     var svg = d3.select('body')
 	.append("svg")
 	.attr('width', c.mapWidth)
@@ -24,13 +31,43 @@ function loadSVG()
 		  svg.attr('transform', d3.event.transform)
 	      }))
 	.append("g")
-
-    d3.xml('19-050_campus_map_revise_v6.svg', function(data) { console.log(data) })
+    d3.xml('19-050_campus_map_revise_v6.svg', function(data) { console.log(data); })
         .then(data => {
-            d3.select('svg g').node().append(data.documentElement)
-            main()
-        })
+            svg.node().append(data.documentElement);
+	    mapSVG = svg.select('svg')
+	    //.attr('viewBox', `0 0 6600 5100`)
+		.attr('preserveAspectRatio', "")
+	    //.attr('width', 1000)
+	    //.attr('height', 800);
+		.attr('style', "")
+		.attr('xml:space', "default")	    
+            main();
+        });
+}
 
+function Save()
+{
+    var body = d3.select('body')
+	.attr('width', c.mapWidth)
+	.attr('height', c.mapHeight);
+    d3.xml('19-050_campus_map_revise_v6.svg', function(data)
+	   {
+	       console.log(data)
+	   })
+        .then(data => {
+	    body.node().append(data.documentElement);
+	    var svg = body.selectAll('svg')
+	    //.attr('width', c.mapWidth)
+	    //.attr('height', c.mapHeight)
+		.attr('transform', `translate(0, 0)`)
+		.call(zoom = d3.zoom()
+		      .scaleExtent([c.minZoomScale, c.maxZoomScale])
+		      .translateExtent([[0,0],[c.mapWidth,c.mapHeight]])
+		      .on('zoom', function () {
+			  svg.attr('transform', d3.event.transform)
+		      }));
+            main();
+        });
 }
 
 function main()
@@ -49,49 +86,37 @@ function makeNest(jsonObject)
     console.log("nested categories" , nestedFalcultyCategories);
 }
 
-function BackButtonLMB(d)
+function BuildingHandlerLMB(bound)
 {
-    console.log("BackButtonLMB entered: ", d);
-}
-
-function BuildingHandlerLMB(building, x, y)
-{
-    // Only scale if current scale is maxed out.
-    //var newX = -x * c.maxZoomScale;
-    //var newY = -y * c.maxZoomScale;
-    //console.log("Old X = ", x, " and old Y = ", y);
-
-    //console.log("BuildingHandler entered: ", building, newX, newY);
-    // Zoom into the building that was clicked, either by manually setting the translate or
-    // making a call using D3's zoom function.
-
-    var bounds = d3.select(building).select('rect');
-    console.log("Bounds: ", bounds.node());
-    newX = bounds.node().x.animVal.value * -1;
-    newY = bounds.node().y.animVal.value * -1;
-    console.log("New X = ", newX, " and new Y = ", newY);
-    console.log("building:", building);
-    console.log("boundParent: ", d3.select(bounds.node().parentNode));
-    d3.select(building).call(zoom.transform, `translate(${newX}, ${newY}) scale(${c.maxZoomScale})`);
+    console.log('id: ', bound.attr('id'));
+    console.log("Mouse Coords: (", mouseX, ", ", mouseY, ")");
+    var boundRect = bound.select('rect').node();
+    console.log("Bound Rect: ", boundRect);
+    
+    /* PRIMITIVE ZOOM CALL */
+    var newX = (mouseX * -1.5);
+    var newY = (mouseY * -1.5);
+    //var newX = (mouseX * -1) - (c.mapWidth / 2);
+    //var newY = (mouseY * -1) - (c.mapHeight / 2);
+    bound.call(zoom.transform, `translate(${newX}, ${newY}) scale(${c.maxZoomScale})`);
 }
 
 function makeClickables()
-{
-    var sel = d3.selectAll('#Buidlings g')
-    sel.on('click', function(d) {
-	var x = d3.mouse(this)[0];
-	var y = d3.mouse(this)[1];
-	BuildingHandlerLMB(this, x, y);
-    });
-
-    // DEBUG Clickable
-    // Only used to manually determine coordinates
-    /*
-    var coordSel = d3.select('body svg');
-    coordSel.on('click', function(d) {
-	var a = d3.mouse(this)[0];
-	var b = d3.mouse(this)[1];
-	console.log("Body-SVG Coords: (", a, ",", b, ")");
-    });
-    */
+{    
+    var sel = d3.select('svg').selectAll('*').filter(function(d, dd, ddd)
+    {
+      id = d3.select(this).attr('id')
+      if((id) && (id.indexOf('-Bounds') !== -1) && (id.indexOf('-Bounds') === (id.length - 7)))
+        return true
+      return false
+    })
+    console.log('sel: ', sel)
+    
+    sel.on('click', function (e, i, p) {
+	var coords = d3.mouse(mapSVG.node());
+	mouseX = coords[0];
+	mouseY = coords[1];
+	//console.log("Click: (", mouseX, ", ", mouseY, ")");
+	BuildingHandlerLMB(d3.select(this));
+    });   
 }
