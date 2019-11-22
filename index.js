95fb3eb7509b6508410dfbdff42a6fb2f4fa5ed0
Will Lucic <wlucic@blue.cs.sonoma.edu>
2019-11-21 18:08:15 -0800

    the 2 zoom methods now work together! Still working on zooming to the right location when not zoomed all the way out

--- index.js
+++ index.js
@@ -7,27 +7,31 @@ const c = {
 };
 
 var zoom;
+var svg;
+var mapSVG;
 
 function loadSVG()
 {
     // Resize body.
     // Call D3's zoom function which also handles translating.
     // Not sure if this will conflict with clicks.
-    var svg = d3.select('body')
+  svg = d3.select('body')
 	.append("svg")
 	.attr('width', c.mapWidth)
 	.attr('height', c.mapHeight)
-	.call(zoom = d3.zoom()
-    .scaleExtent([c.minZoomScale, c.maxZoomScale])
-    .translateExtent([[0,0],[c.mapWidth,c.mapHeight]])
-    .on('zoom', function () {
-      svg.attr('transform', d3.event.transform)
-    }))
 	.append("g")
+  // zoom = d3.zoom()
+  //   .scaleExtent([c.minZoomScale, c.maxZoomScale])
+  //   .translateExtent([[0,0],[c.mapWidth,c.mapHeight]])
+  //   .on('zoom', function () {
+  //     svg.attr('transform', d3.event.transform)
+  //   })
+  // d3.select('body svg').call(zoom)
 
   d3.xml('map.svg', function(data) { console.log(data) })
     .then(data => {
       svg.node().append(data.documentElement)
+      mapSVG = svg.select('svg')
       main()
     })
 
@@ -35,6 +39,13 @@ function loadSVG()
 
 function main()
 {
+  zoom = d3.zoom()
+    .scaleExtent([c.minZoomScale, c.maxZoomScale])
+    .translateExtent([[0,0],[c.mapWidth,c.mapHeight]])
+    .on('zoom', function () {
+      d3.select('body svg g').attr('transform', d3.event.transform)
+    })
+  d3.select('body svg').call(zoom)
   makeClickables();
 }
 
@@ -55,24 +66,49 @@ function BackButtonLMB(d)
 
 function BuildingHandlerLMB(bound)
 {
-  console.log('bound: ', bound)
-  console.log('bound node BB: ', bound.node().getBBox())
-  console.log('id: ', bound.attr('id'))
+  var centroid = getCentroid(bound.node().getBBox())
+
+  d3.select(bound.node().parentNode).append("circle").attr("cx", centroid.x).attr("cy", centroid.y).attr("r", 3).style("fill", "purple").attr('opacity', 1);
+
+
+  var winWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  var winHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  // console.log('w: ', winWidth,'h: ', winHeight)
+  // console.log('centroid before: ', centroid)
+  var mapDims = mapSVG.node().viewBox.baseVal
+  // centroid.x = ((centroid.x) - mapDims.width)// / mapDims.width * winWidth
+  // centroid.y =  ((centroid.y) - mapDims.height)// / mapDims.height * winHeight
+  // console.log('body selection/node: ', d3.zoomTransform(d3.select('body svg').node()).k)
+  var curZoom = d3.zoomTransform(d3.select('body svg').node())
+  console.log('curZoom: k=', curZoom.k, ' x: ', curZoom.x, ' y: ', curZoom.y )
+  console.log('centroid-pre: ', centroid)
+  centroid.x = (centroid.x - curZoom.x)/curZoom.k
+  centroid.y = (centroid.y - curZoom.y)/curZoom.k
+  console.log('centroid-post: ', centroid)
+  // console.log('centroid after: ', centroid)
+  var translation = d3.zoomIdentity.scale(c.maxZoomScale).translate(-centroid.x, -centroid.y)
+
+  d3.select('body svg').transition().duration(2000).call(zoom.transform, translation)
+
 }
 
 function makeClickables()
 {
-    var sel = d3.select('svg').selectAll('*').filter(function(d, dd, ddd)
+    var sel = d3.select('svg').selectAll('*').filter(function()
     {
-      id = d3.select(this).attr('id')
-      if((id) && (id.indexOf('-Bounds') !== -1) && (id.indexOf('-Bounds') === (id.length - 7)))
-        return true
+      var id = d3.select(this).attr('id')
+      var pos
+      if(id)
+      {
+        var pos = (id.indexOf('-Bounds'))
+        if ((pos !== -1) && (pos === (id.length - 7)))
+          return true
+      }
       return false
     })
     console.log('sel: ', sel)
 
-    sel.on('click', function (e, i, p)
-    {
-      BuildingHandlerLMB(d3.select(this))
-    })
+    sel.on('click', function () { BuildingHandlerLMB(d3.select(this)) })
 }
+
+function getCentroid(rect) { return { x:rect.x + rect.width/2, y:rect.y + rect.height/2} }
