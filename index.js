626683489109a50d2385ede77ed15d5e0c73ea10
mbaello <33732855+mbaello@users.noreply.github.com>
2019-11-22 01:54:13 -0800

    Zoom Implemented
    -Modified some zoom logic.
    -Added offsets to control where to place the centroid on the window (i.e. where to zoom into).

--- index.js
+++ index.js
@@ -1,148 +1,134 @@
 // Constants for use throughout the program.
 const c = {
-    mapWidth: 1000,
-    mapHeight: 800,
+    // Old width and height were 1000 and 800, respectively.
+    mapWidth: 792,
+    mapHeight: 612,
     minZoomScale: 1,
     maxZoomScale: 6,
-    svgVBWidth: 3300,
-    svgVBHeight: 2550,
-    zDuration: 1000,
-    zEase: d3.easeCubicOut
+    mapVBWidth: 792,
+    mapVBHeight: 612
 };
 
 var zoom;
-var nestedSSUData;
+var svg;
 var mapSVG;
-var mouseX;
-var mouseY;
 
 function loadSVG()
 {
     // Resize body.
     // Call D3's zoom function which also handles translating.
-    // Not sure if this will conflict with clicks.   	
-    var svg = d3.select('body')
+    // Not sure if this will conflict with clicks.
+    svg = d3.select('body')
 	.append("svg")
 	.attr('width', c.mapWidth)
 	.attr('height', c.mapHeight)
-	.call(zoom = d3.zoom()
-	      .scaleExtent([c.minZoomScale, c.maxZoomScale])
-              .translateExtent([[0,0],[c.mapWidth,c.mapHeight]])
-	      .on('zoom', function () {
-		  svg.attr('transform', d3.event.transform)
-	      }))
 	.append("g")
-    d3.xml('19-050_campus_map_revise_v6.svg', function(data) { console.log(data); })
-        .then(data => {
-            svg.node().append(data.documentElement);
+	.attr('width', c.mapVBWidth)
+	.attr('height', c.mapVBHeight);
+    // zoom = d3.zoom()
+    //   .scaleExtent([c.minZoomScale, c.maxZoomScale])
+    //   .translateExtent([[0,0],[c.mapWidth,c.mapHeight]])
+    //   .on('zoom', function () {
+    //     svg.attr('transform', d3.event.transform)
+    //   })
+    // d3.select('body svg').call(zoom)
+    
+    d3.xml('map.svg', function(data) { console.log(data) })
+	.then(data => {
+	    svg.node().append(data.documentElement)
 	    mapSVG = svg.select('svg')
-	    //.attr('viewBox', `0 0 6600 5100`)
-		.attr('preserveAspectRatio', "")
 	    //.attr('width', 1000)
-	    //.attr('height', 800);
-		.attr('style', "")
-		.attr('xml:space', "default")	    
-            main();
-        });
-}
+	    //.attr('height', 800)
+	    //.attr('viewBox', '0 0 1000 800')
+	    main();
+	})
 
-function Save()
-{
-    var body = d3.select('body')
-	.attr('width', c.mapWidth)
-	.attr('height', c.mapHeight);
-    d3.xml('19-050_campus_map_revise_v6.svg', function(data)
-	   {
-	       console.log(data)
-	   })
-        .then(data => {
-	    body.node().append(data.documentElement);
-	    var svg = body.selectAll('svg')
-	    //.attr('width', c.mapWidth)
-	    //.attr('height', c.mapHeight)
-		.attr('transform', `translate(0, 0)`)
-		.call(zoom = d3.zoom()
-		      .scaleExtent([c.minZoomScale, c.maxZoomScale])
-		      .translateExtent([[0,0],[c.mapWidth,c.mapHeight]])
-		      .on('zoom', function () {
-			  svg.attr('transform', d3.event.transform)
-		      }));
-            main();
-        });
 }
 
 function main()
 {
+    zoom = d3.zoom()
+	.scaleExtent([c.minZoomScale, c.maxZoomScale])
+	.translateExtent([[0,0],[c.mapWidth,c.mapHeight]])
+	.on('zoom', function () {
+	    d3.select('body svg g').attr('transform', d3.event.transform)
+	});
+    d3.select('body svg').call(zoom);
     makeClickables();
 }
 
 function makeNest(jsonObject)
 {
-    nestedSSUData = d3.nest()
-	.key(d => d['Building'])
-	.key(d => d['Department'])
-	.key(d => d['L_Name'])
-	.entries(jsonObject);
-    
-    console.log("nested categories" , nestedSSUData);
+    const nestedFalcultyCategories = d3.nest()
+	  .key(d => d['Building'])
+	  .key(d => d['Department'])
+	  .key(d => d['L_Name'])
+	  .entries(jsonObject)
+    console.log("nested categories" , nestedFalcultyCategories);
+}
+
+function BackButtonLMB(d)
+{
+    console.log("BackButtonLMB entered: ", d);
 }
 
 function BuildingHandlerLMB(bound)
 {
-    // Save the bound's ID to a variable
-    var id = bound.attr('id');
+    var centroid = getCentroid(bound.node().getBBox())
 
-    /* TESTING NESTED DATA RETRIEVAL */
-    
-    // Remove the bounds from the ID
-    id = id.replace("-Bounds", "");
-    // Remove the hyphens from the ID
-    id = id.replace("-", " ");
-    var i;
-    for(i = 0; i < nestedSSUData.length; i++) {
-	if(nestedSSUData[i].key == id) {
-	    console.log("Match found for", id);
-	    // Break to keep the index of the matching key
-	    break;
-	}
+    d3.select(bound.node().parentNode).append("circle").attr("cx", centroid.x).attr("cy", centroid.y).attr("r", 3).style("fill", "purple").attr('opacity', 1);
+
+
+    var winWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var winHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    // console.log('w: ', winWidth,'h: ', winHeight)
+    // console.log('centroid before: ', centroid)
+    var mapDims = mapSVG.node().viewBox.baseVal;
+    // centroid.x = ((centroid.x) - mapDims.width)// / mapDims.width * winWidth
+    // centroid.y =  ((centroid.y) - mapDims.height)// / mapDims.height * winHeight
+    // console.log('body selection/node: ', d3.zoomTransform(d3.select('body svg').node()).k)
+    var curZoom = d3.zoomTransform(d3.select('body svg').node());
+    // curZoom.k is the current scale factor when clicked. x and y is presumably the
+    // top-left-most point visible at the zoom factor k.
+    //console.log('curZoom: k=', curZoom.k, ' x: ', curZoom.x, ' y: ', curZoom.y )
+    console.log('centroid-pre: ', centroid);
+    var offsetX = (c.mapVBWidth / 2);
+    var offsetY = (c.mapVBHeight / 2);
+    /*
+    if(curZoom.k == 1)
+    {
+	console.log('Modifying centroid');
+	centroid.x = (centroid.x - curZoom.x)/curZoom.k;
+	centroid.y = (centroid.y - curZoom.y)/curZoom.k;
     }
-    // At this point, if the index is valid, display the relevant majors for the
-    // target building. 
-    if(i < nestedSSUData.length) {
-	console.log("LISTING MAJORS/AREAS");
-	for(j = 0; j < nestedSSUData[i].values.length; j++) {
-	    console.log(nestedSSUData[i].values[j]);
-	}
-    }
-    console.log('id: ', id);
-    console.log("Mouse Coords: (", mouseX, ", ", mouseY, ")");
-    var boundRect = bound.select('rect').node();
-    console.log("Bound Rect: ", boundRect);
+    */
+    console.log('centroid-post: ', centroid);
+    //console.log('offsetX = ', offsetX, '; offsetY = ', offsetY);
+    // console.log('centroid after: ', centroid)
+    var translation = d3.zoomIdentity.scale(c.maxZoomScale).translate(-centroid.x, -centroid.y)
+    //console.log('translate = ', translation);
+    translation.x += offsetX;
+    translation.y += offsetY;
+    d3.select('body svg').transition().duration(1500).call(zoom.transform, translation);
     
-    /* PRIMITIVE ZOOM - We still need to get the centroid/proper bounding box coords... */
-    var newX = (mouseX * -1.5);
-    var newY = (mouseY * -1.5);
-    //var newX = (mouseX * -1) - (c.mapWidth / 2);
-    //var newY = (mouseY * -1) - (c.mapHeight / 2);
-    bound.call(zoom.transform, `translate(${newX}, ${newY}) scale(${c.maxZoomScale})`);
 }
 
 function makeClickables()
-{    
-    var sel = d3.select('svg').selectAll('*').filter(function(d, dd, ddd)
-    {
-      id = d3.select(this).attr('id')
-      if((id) && (id.indexOf('-Bounds') !== -1) && (id.indexOf('-Bounds') === (id.length - 7)))
-        return true
-      return false
+{
+    var sel = d3.select('svg').selectAll('*').filter(function() {
+	var id = d3.select(this).attr('id')
+	var pos
+	if(id)
+	{
+	    var pos = (id.indexOf('-Bounds'))
+	    if ((pos !== -1) && (pos === (id.length - 7)))
+		return true
+	}
+	return false
     })
     console.log('sel: ', sel)
-    
-    sel.on('click', function (e, i, p) {
-	var coords = d3.mouse(mapSVG.node());
-	mouseX = coords[0];
-	mouseY = coords[1];
-	//console.log("Click: (", mouseX, ", ", mouseY, ")");
-	BuildingHandlerLMB(d3.select(this));
-    });   
+
+    sel.on('click', function () { BuildingHandlerLMB(d3.select(this)) })
 }
+
+function getCentroid(rect) { return { x:rect.x + rect.width/2, y:rect.y + rect.height/2} }
