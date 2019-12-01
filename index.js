a7e2394307fdc7252badcf8bb0a303e8c7d196c3
Will Lucic <wlucic@blue.cs.sonoma.edu>
2019-11-30 16:19:25 -0800

    about to switch to local lucic and merge

--- index.js
+++ index.js
@@ -79,45 +79,45 @@ function BackButtonLMB(d)
 }
 
 
-function BuildingHandlerLMB(bound)
-{
-    var centroid = getCentroid(bound.node().getBBox())
-
-    //d3.select(bound.node().parentNode).append("circle").attr("cx", centroid.x).attr("cy", centroid.y).attr("r", 3).style("fill", "purple").attr('opacity', 1);
-
-
-    var winWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    var winHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    // console.log('w: ', winWidth,'h: ', winHeight)
-    // console.log('centroid before: ', centroid)
-    var mapDims = mapSVG.node().viewBox.baseVal;
-    // centroid.x = ((centroid.x) - mapDims.width)// / mapDims.width * winWidth
-    // centroid.y =  ((centroid.y) - mapDims.height)// / mapDims.height * winHeight
-    // console.log('body selection/node: ', d3.zoomTransform(d3.select('body svg').node()).k)
-    var curZoom = d3.zoomTransform(d3.select('body svg').node());
-    // curZoom.k is the current scale factor when clicked. x and y is presumably the
-    // top-left-most point visible at the zoom factor k.
-    //console.log('curZoom: k=', curZoom.k, ' x: ', curZoom.x, ' y: ', curZoom.y )
-    console.log('centroid-pre: ', centroid);
-    var offsetX = (c.mapVBWidth / 3);
-    var offsetY = (c.mapVBHeight / 2);
-    /*
-    if(curZoom.k == 1)
-    {
-	console.log('Modifying centroid');
-	centroid.x = (centroid.x - curZoom.x)/curZoom.k;
-	centroid.y = (centroid.y - curZoom.y)/curZoom.k;
-    }
-    */
-    console.log('centroid-post: ', centroid);
-    //console.log('offsetX = ', offsetX, '; offsetY = ', offsetY);
-    // console.log('centroid after: ', centroid)
-    var translation = d3.zoomIdentity.scale(c.maxZoomScale).translate(-centroid.x, -centroid.y)
-    //console.log('translate = ', translation);
-    translation.x += offsetX;
-    translation.y += offsetY;
-    d3.select('body svg').transition().duration(1500).call(zoom.transform, translation);
-}
+// function BuildingHandlerLMB(bound)
+// {
+//     var centroid = getCentroid(bound.node().getBBox())
+//
+//     //d3.select(bound.node().parentNode).append("circle").attr("cx", centroid.x).attr("cy", centroid.y).attr("r", 3).style("fill", "purple").attr('opacity', 1);
+//
+//
+//     var winWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+//     var winHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+//     // console.log('w: ', winWidth,'h: ', winHeight)
+//     // console.log('centroid before: ', centroid)
+//     var mapDims = mapSVG.node().viewBox.baseVal;
+//     // centroid.x = ((centroid.x) - mapDims.width)// / mapDims.width * winWidth
+//     // centroid.y =  ((centroid.y) - mapDims.height)// / mapDims.height * winHeight
+//     // console.log('body selection/node: ', d3.zoomTransform(d3.select('body svg').node()).k)
+//     var curZoom = d3.zoomTransform(d3.select('body svg').node());
+//     // curZoom.k is the current scale factor when clicked. x and y is presumably the
+//     // top-left-most point visible at the zoom factor k.
+//     //console.log('curZoom: k=', curZoom.k, ' x: ', curZoom.x, ' y: ', curZoom.y )
+//     console.log('centroid-pre: ', centroid);
+//     var offsetX = (c.mapVBWidth / 3);
+//     var offsetY = (c.mapVBHeight / 2);
+//     /*
+//     if(curZoom.k == 1)
+//     {
+// 	console.log('Modifying centroid');
+// 	centroid.x = (centroid.x - curZoom.x)/curZoom.k;
+// 	centroid.y = (centroid.y - curZoom.y)/curZoom.k;
+//     }
+//     */
+//     console.log('centroid-post: ', centroid);
+//     //console.log('offsetX = ', offsetX, '; offsetY = ', offsetY);
+//     // console.log('centroid after: ', centroid)
+//     var translation = d3.zoomIdentity.scale(c.maxZoomScale).translate(-centroid.x, -centroid.y)
+//     //console.log('translate = ', translation);
+//     translation.x += offsetX;
+//     translation.y += offsetY;
+//     d3.select('body svg').transition().duration(1500).call(zoom.transform, translation);
+// }
 
 function makeClickables()
 {
@@ -134,8 +134,54 @@ function makeClickables()
     })
     console.log('sel: ', sel)
 
-    sel.on('click', function () { BuildingHandlerLMB(d3.select(this)) })
+    sel.on('click', function () { zoomToSelection(d3.select(this), 1/3, 1/2) })
 }
+
+function zoomToSelection(id, xRat, yRat)
+{
+  var rect = id.node().getBBox()
+  var centroid =
+  {
+    x: rect.x + rect.width/2,
+    y: rect.y + rect.height/2
+  }
+
+  if(xRat === null) xRat = 0.5
+  else if (xRat < 0) xRat = 0
+  else if(xRat > 1) xRat = 1
+
+  if(yRat === null) yRat = 0.5
+  else if (yRat < 0) yRat = 0
+  else if(yRat > 1) yRat = 1
+
+  var offsetX = (c.mapVBWidth * xRat)
+  var offsetY = (c.mapVBHeight * yRat)
+
+  var translation = d3.zoomIdentity.scale(c.maxZoomScale).translate(-centroid.x, -centroid.y)
+
+  translation.x += offsetX
+  translation.y += offsetY
+
+  d3.select('body svg').transition().duration(500).call(zoom.transform, translation)
+
+}
+
+function zoomByName(buildingName, xRat, yRat)
+{
+  var buildingId = formatToBoundId(buildingName)
+  var sel = d3.select(`svg g svg #${buildingId}`);
+  if(!sel.empty())
+    zoomToSelection(sel, xRat, yRat);
+}
+
+function formatToBoundId(buildingName)
+{
+  var outString = buildingName + '-Bounds'
+  outString = outString.replace(' ', '-')
+  console.log('outstring: ', outString)
+  return outString
+}
+
 
 function makeSearchBox()
 {
@@ -169,6 +215,7 @@ function textInputHandler(event)
   {
     console.log('entered: ', event.path[0].value);
     // console.log('event: ', event)
+    zoomByName(event.path[0].value, 1/3, 1/2);
 
   }
 }
